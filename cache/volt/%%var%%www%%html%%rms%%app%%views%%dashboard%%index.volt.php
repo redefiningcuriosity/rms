94bb57f761dd1ec794e987795224421e0d0ecadc
redefiningcuriosity <redefiningcuriosity@gmail.com>
2015-11-09 16:18:41 +0530

    Customer panel half done

---
+++ cache/volt/%%var%%www%%html%%rms%%app%%views%%dashboard%%index.volt.php
@@ -0,0 +1,39 @@
+<?php echo $this->getContent(); ?>
+<style>
+#hoverid:hover {do:background-color: #EEE}
+</style>
+<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">			
+		<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="#"><span class="glyphicon glyphicon-home"></span></a></li>
+				<li class="active">Dashboard</li>
+			</ol>
+		</div><!--/.row-->
+		
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Dashboard</h1>
+			</div>
+		</div><!--/.row-->
+		<div class="row">
+			<div class="col-xs-12 col-md-6 col-lg-3">
+				
+			</div>
+			<div class="col-xs-24 col-md-12 col-lg-6" id="hoverid" >
+				<div class="panel panel-blue panel-widget nav " >
+				<a href="">
+					<div class="row no-padding">
+						<div class="col-sm-2 col-lg-2 widget-left">
+							<em class="glyphicon glyphicon-shopping-cart glyphicon-l"></em>
+						</div>
+						<div class="col-sm-9 col-lg-7 widget-right" >
+							<div class="large">Hungry kya?</div>
+							<div class="text-muted">Place a new order.</div>
+						</div>
+					</div>
+				</a>
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-6 col-lg-3">
+		</div><!--/.row-->
+		
--- cache/volt/%%var%%www%%html%%rms%%app%%views%%layouts%%sidebar.volt.php
+++ cache/volt/%%var%%www%%html%%rms%%app%%views%%layouts%%sidebar.volt.php
@@ -1,40 +1,39 @@
-	<div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
+	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sidebar-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="dashboard"><span>RMS</span> Panel</a>
+				<ul class="user-menu">
+					<li class="dropdown pull-right">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> <?php echo $name; ?> <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="profile"><span class="glyphicon glyphicon-user"></span> Profile</a></li>
+							<li><a href="settings"><span class="glyphicon glyphicon-cog"></span> Settings</a></li>
+							<li><a href="logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+							
+		</div><!-- /.container-fluid -->
+	</nav>
+<div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
 		<form role="search">
 			<div class="form-group">
 				<input type="text" class="form-control" placeholder="Search">
 			</div>
 		</form>
 		<ul class="nav menu">
-			<li class="active"><a href="index.html"><span class="glyphicon glyphicon-dashboard"></span> Dashboard</a></li>
-			<li><a href="widgets.html"><span class="glyphicon glyphicon-th"></span> Widgets</a></li>
-			<li><a href="charts.html"><span class="glyphicon glyphicon-stats"></span> Charts</a></li>
-			<li><a href="tables.html"><span class="glyphicon glyphicon-list-alt"></span> Tables</a></li>
-			<li><a href="forms.html"><span class="glyphicon glyphicon-pencil"></span> Forms</a></li>
-			<li><a href="panels.html"><span class="glyphicon glyphicon-info-sign"></span> Alerts &amp; Panels</a></li>
-			<li class="parent ">
-				<a href="#">
-					<span class="glyphicon glyphicon-list"></span> Dropdown <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="glyphicon glyphicon-s glyphicon-plus"></em></span> 
-				</a>
-				<ul class="children collapse" id="sub-item-1">
-					<li>
-						<a class="" href="#">
-							<span class="glyphicon glyphicon-share-alt"></span> Sub Item 1
-						</a>
-					</li>
-					<li>
-						<a class="" href="#">
-							<span class="glyphicon glyphicon-share-alt"></span> Sub Item 2
-						</a>
-					</li>
-					<li>
-						<a class="" href="#">
-							<span class="glyphicon glyphicon-share-alt"></span> Sub Item 3
-						</a>
-					</li>
-				</ul>
-			</li>
+			<li <?php if ($conname == 'Dashboard') { ?> class="active" <?php } ?>><a href="dashboard"><span class="glyphicon glyphicon-dashboard"></span> Dashboard</a></li>
+			<li <?php if ($conname == 'Cmenu') { ?> class="active" <?php } ?>><a href="cmenu"><span class="glyphicon glyphicon-th"></span> Menu</a></li>
+			<li <?php if ($conname == 'Orders') { ?> class="active" <?php } ?>><a href="corders"><span class="glyphicon glyphicon-list-alt"></span> Orders</a></li>
+			<li <?php if ($conname == 'Contact') { ?> class="active" <?php } ?>><a href="contact"><span class="glyphicon glyphicon-pencil"></span> Contact Us </a></li>
 			<li role="presentation" class="divider"></li>
-			<li><a href="login.html"><span class="glyphicon glyphicon-user"></span> Login Page</a></li>
 		</ul>
 	</div><!--/.sidebar-->
 <?php echo $this->getContent(); ?>
